41e517ab945078a2bbbb9f00b11e6562f87dd60a
dhaval-metaliya <champ.team.una@gmail.com>
2026-04-23 09:50:06 +0530

    events.js

--- functions/api/events.js
+++ functions/api/events.js
@@ -1,18 +1,17 @@
-export async function onRequestGet({ request, env }) {
-
-  const cookie = request.headers.get("cookie") || "";
-
-  if (!cookie.includes("auth=1")) {
-    return new Response("Unauthorized", { status: 401 });
-  }
+export async function onRequestGet({ env }) {
 
   const list = await env.VISITOR_KV.list({ limit: 100 });
 
-  const data = await Promise.all(
-    list.keys.map(k => env.VISITOR_KV.get(k.name))
-  );
+  const data = [];
 
-  return new Response(JSON.stringify(data.map(JSON.parse)), {
+  for (const key of list.keys) {
+    if (key.name.startsWith("sent_")) continue;
+
+    const val = await env.VISITOR_KV.get(key.name);
+    if (val) data.push(JSON.parse(val));
+  }
+
+  return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" }
   });
 }
